a3e1978f7bd47c00ae320db048bf6e9380e2aa29
chojsm <chojs28@gmail.com>
2021-12-09 15:15:48 +0900

    Back API 로깅 (heroku 확인용)

--- routes/goCamping/basedList.ts
+++ routes/goCamping/basedList.ts
@@ -7,27 +7,44 @@ const request = require('request');
 const baseURL = 'http://api.visitkorea.or.kr/openapi/service/rest/GoCamping/basedList?';
 
 router.get('/', (req, res) => {
-    const { pageNo, numOfRows } = req.query;
+  const { pageNo, numOfRows } = req.query;
 
-    let queryParams = 
-    encodeURIComponent('ServiceKey') + '=' + process.env.SERVICE_KEY + '&' +
-    encodeURIComponent('MobileOS') + '=' + encodeURIComponent('ETC') + '&' +
-    encodeURIComponent('MobileApp') + '=' + encodeURIComponent('AppTest') + '&' +
-    encodeURIComponent('pageNo') + '=' + encodeURIComponent(String(pageNo)) + '&' +
-    encodeURIComponent('numOfRows') + '=' + encodeURIComponent(String(numOfRows)) + '&' +
-    '_type=json';  
+  let queryParams =
+    encodeURIComponent('ServiceKey') +
+    '=' +
+    process.env.SERVICE_KEY +
+    '&' +
+    encodeURIComponent('MobileOS') +
+    '=' +
+    encodeURIComponent('ETC') +
+    '&' +
+    encodeURIComponent('MobileApp') +
+    '=' +
+    encodeURIComponent('AppTest') +
+    '&' +
+    encodeURIComponent('pageNo') +
+    '=' +
+    encodeURIComponent(String(pageNo)) +
+    '&' +
+    encodeURIComponent('numOfRows') +
+    '=' +
+    encodeURIComponent(String(numOfRows)) +
+    '&' +
+    '_type=json';
 
-    request(
-        {
-            url: baseURL + queryParams,
-            method: 'GET',
-        },
-    
-        (err, response, body) => {
-            res.send(body);
-        }
-    )
-})
+  console.log('basedList queryParams', queryParams);
+
+  request(
+    {
+      url: baseURL + queryParams,
+      method: 'GET',
+    },
+
+    (err, response, body) => {
+      console.log('basedList responseBody', body);
+      res.send(body);
+    },
+  );
+});
 
 module.exports = router;
-
