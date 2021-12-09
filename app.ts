2e561d73c144c855c6e690aeb14792e7c62ccc17
chojsm <chojs28@gmail.com>
2021-12-09 20:29:16 +0900

    Back 라우팅 테스트

--- app.ts
+++ app.ts
@@ -6,7 +6,6 @@ import * as path from 'path';
 // ====================================
 // router import
 // index
-const request = require('request');
 const indexRouter = require('./routes/index');
 
 // goCamping
@@ -30,54 +29,13 @@ app.use(cors());
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/test', (req, res) => {
-  res.send({
-    data: 'test',
-  });
-});
-
-app.get('/goCamping/basedList', (req, res) => {
-  const baseURL = 'http://api.visitkorea.or.kr/openapi/service/rest/GoCamping/imageList?';
-  const { contentId } = req.query;
-
-  let queryParams =
-    encodeURIComponent('ServiceKey') +
-    '=' +
-    process.env.SERVICE_KEY +
-    '&' +
-    encodeURIComponent('MobileOS') +
-    '=' +
-    encodeURIComponent('ETC') +
-    '&' +
-    encodeURIComponent('MobileApp') +
-    '=' +
-    encodeURIComponent('AppTest') +
-    '&' +
-    encodeURIComponent('contentId') +
-    '=' +
-    encodeURIComponent(String(contentId)) +
-    '&' +
-    '_type=json';
-
-  request(
-    {
-      url: baseURL + queryParams,
-      method: 'GET',
-    },
-
-    (err, response, body) => {
-      res.send(body);
-    },
-  );
-});
-
 // ====================================
 // routes
 // index
 app.use('/', indexRouter);
 
 // goCamping
-// app.use('/goCamping/basedList', basedList);
+app.use('/goCamping/basedList', basedList);
 app.use('/goCamping/imageList', imageList);
 app.use('/goCamping/locationBasedList', locationBasedList);
 app.use('/goCamping/searchList', searchList);
