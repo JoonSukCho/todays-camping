c82f97da168f6179f8ce08118388eb4ac639674c
chojsm <chojs28@gmail.com>
2021-12-09 20:46:10 +0900

    Back 라우팅 테스트

--- app.ts
+++ app.ts
@@ -48,7 +48,7 @@ app.listen(process.env.PORT || 3001, () => {
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'FrontEnd/build')));
-  app.get('/*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '../FrontEnd/build/index.html'));
   });
 }
