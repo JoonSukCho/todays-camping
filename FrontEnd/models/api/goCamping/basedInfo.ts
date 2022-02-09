f920a8f96a589fbd4f57ef0939239575a5e3a654
chojsm <chojs28@gmail.com>
2022-02-09 11:48:47 +0900

    feat: 검색 페이지 구현 진행중

--- FrontEnd/models/api/goCamping/basedInfo.ts
+++ FrontEnd/models/api/goCamping/basedInfo.ts
@@ -1,4 +1,4 @@
-import { _iGCResponseBody } from 'models/api/goCamping/common';
+import { _iCommonItem, _iGCResponseBody } from 'models/api/goCamping/common';
 
 // types
 export type _tBasedItem = _iBasedItem[] | _iBasedItem;
@@ -22,29 +22,4 @@ export interface _iBasedInfo {
   itemList: _iBasedItem[];
 }
 
-export interface _iBasedItem {
-  contentId: number; // Image랑 mapping에 필요한 id
-  facltNm?: string; // 야영장명
-  lineIntro?: string; // 한줄소개
-  intro?: string; // 소개
-  addr1?: string; // 주소
-  addr2?: string; // 상세주소
-  manageSttus?: string; // 운영상태, 관리상태
-  featureNm?: any; // 특징
-  induty?: string; // 업종
-  mapX?: number; // 경도(X)
-  mapY?: number; // 위도(Y)
-  tel?: string; // 전화번호
-  homepage?: string; // 홈페이지 url
-  resveUrl?: string; // 예약페이지
-  firstImageUrl?: string; // 대표이미지 url
-  siteBottomCl1?: number; // 잔디
-  siteBottomCl2?: number; // 파쇄석
-  siteBottomCl3?: number; // 테크
-  siteBottomCl4?: number; // 자갈
-  siteBottomCl5?: number; // 맨흙
-  sbrsCl?: string; // 부대시설
-  animalCmgCl?: string; // 애완동물 출입
-  operPdCl?: string; // 운영기간
-  operDeCl?: string; // 운영일
-}
+export interface _iBasedItem extends _iCommonItem {}
--- FrontEnd/pages/search-result.tsx
+++ FrontEnd/pages/search-result.tsx
@@ -1,12 +1,129 @@
+import { Button, TextField } from '@material-ui/core';
+import GridContainer from 'components/Grid/GridContainer';
+import GridItem from 'components/Grid/GridItem';
 import Header from 'components/Header/Header';
 import HeaderLinks from 'components/Header/HeaderLinks';
+import Parallax from 'components/Parallax/Parallax';
+import ParallaxContent from 'components/Parallax/ParallaxContent';
+import MainSection from 'components/Section/MainSection';
+import useSearchInfo from 'Hooks/api/useSearchInfo';
+import { _iSearchInfoReqParams } from 'models/api/goCamping/searchInfo';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { removeAllBlank } from 'util/utils';
 
 const SearchResult = () => {
+  const [keyword, setKeyword] = useState<string>('');
+  const [searchInfoReqParams, setSearchInfoReqParams] =
+    useState<_iSearchInfoReqParams>(null);
+
+  const {
+    data: searchInfo,
+    error: searchInfoError,
+    isFetched: searchInfoIsFetched,
+    refetch: getSearchInfo,
+  } = useSearchInfo(searchInfoReqParams);
+
+  const inputChangeHandler = (e) => {
+    setKeyword(e.target.value);
+  };
+
+  const searchHandler = () => {
+    setSearchInfoReqParams({
+      numOfRows: 10,
+      pageNo: 1,
+      keyword,
+    });
+  };
+
+  useEffect(() => {
+    if (searchInfoReqParams) {
+      getSearchInfo();
+    }
+  }, [searchInfoReqParams]);
+
+  useEffect(() => {
+    if (searchInfoIsFetched) {
+      console.log(keyword, searchInfo);
+    }
+  }, [searchInfoIsFetched]);
+
   return (
     <>
-      <Header brand="오늘의 캠핑" rightLinks={<HeaderLinks />} fixed color="black" />
+      <Header
+        brand="오늘의 캠핑"
+        rightLinks={<HeaderLinks />}
+        fixed
+        color="black"
+      />
+      <Parallax height={500} bgColor="rgba(0, 0, 0, 0.8)">
+        <ParallaxContent>
+          <GridContainer>
+            <GridItem>
+              {/* <TitleContainer>
+                <Title>키워드 검색</Title>
+              </TitleContainer> */}
+              <div
+                style={{
+                  padding: '20px 0px 20px 0px',
+                  marginTop: '25px',
+                  borderRadius: '8px',
+                  paddingBottom: '25px',
+                }}
+              >
+                <TextField
+                  placeholder="키워드 검색"
+                  variant="outlined"
+                  onChange={inputChangeHandler}
+                  style={{ background: '#fff', borderRadius: 8, width: '100%' }}
+                  InputProps={{
+                    endAdornment: (
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={searchHandler}
+                      >
+                        검색
+                      </Button>
+                    ),
+                  }}
+                />
+              </div>
+            </GridItem>
+          </GridContainer>
+        </ParallaxContent>
+      </Parallax>
+      <MainSection>
+        <div style={{ height: 1000 }}>Section </div>
+      </MainSection>
     </>
   );
 };
+
+const TitleContainer = styled.div`
+  color: #ffffff;
+  text-align: left;
+`;
+
+const Title = styled.h1`
+  font-size: 3.2rem;
+  font-weight: 600;
+  display: inline-block;
+  position: relative;
+
+  @media (max-width: 576px) {
+    font-size: 2.5rem;
+  }
+`;
+
+const SubTitle = styled.h3`
+  font-size: 1.313rem;
+  max-width: 500px;
+  margin: 10px 0 0;
+
+  @media (max-width: 576px) {
+    font-size: 1rem;
+  }
+`;
 
 export default SearchResult;
