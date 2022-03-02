5796a1a45c7fc16ac71bef59bd4a1c3d430f8ae2
chojsm <chojs28@gmail.com>
2022-03-02 14:40:41 +0900

    test: Api 로그 남김, axios default 정의를 saga에서

--- FrontEnd/sagas/index.ts
+++ FrontEnd/sagas/index.ts
@@ -1,6 +1,13 @@
+import axios from 'axios';
 import { all, fork } from 'redux-saga/effects';
 import userSaga from 'sagas/user';
 import likeListSaga from 'sagas/likeList';
+
+axios.defaults.withCredentials = true;
+axios.defaults.headers = {
+  'Content-Type': 'application/json',
+  Accept: 'application/json',
+};
 
 export default function* rootSaga() {
   yield all([fork(userSaga), fork(likeListSaga)]);
